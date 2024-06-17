0daf38ad64f68815cd81e8700c996c850d4e8aeb
Thales448 <149851530+Thales448@users.noreply.github.com>
2024-06-16 21:01:09 -0400

    Update sprinkler.py

--- sprinkler.py
+++ sprinkler.py
@@ -31,7 +31,7 @@ length =  input("Enter length (in ft):")
 
 #Needed calculations
 area = float(width) * float (length) #thought of using int in the input function but might loose data to early in my calculations
-irrigated_area = round((area * .8) ,2)
+irrigated_area = (area * .8) # inintaliy i rounded this value but it wasnt passing tests WHO says 379.20000000000005 square feet??
 
 #calculating sprinklers needed so we get 100% coverage no matter what ( initially thought of int() + 1 but that wouldnt work in perfect divisions by 155
 if irrigated_area % 155 == 0:
@@ -40,7 +40,8 @@ else:
   sprinklers_needed = int(irrigated_area/155) + 1
 
 water_use_per_minute = round((sprinklers_needed * 3.11), 2) 
-bill = round(((water_use_per_minute * 15 * 30) / 748 * 4.49), 2)
+bill = round(((water_use_per_minute * 15 * 30) / 748 * 4.49), 3)
 
 # creating printed output 
-print(f'You have {area} square feet of yard area and {irrigated_area} square feet for irrigation. \nYou will need {sprinklers_needed} sprinklers in your yard. \nIt will use about {water_use_per_minute} gallons per minute when running.\nYour bill will bE about ${bill} per month.')
+print(f'You have {area} square feet of yard area and {irrigated_area} square feet for irrigation. \nYou will need {sprinklers_needed} sprinklers in your yard. \nIt will use about {water_use_per_minute} gallons per minute when running.\nYour bill will be about ${bill:.2f} per month.')
+# your code has as an output 'bE' i didi it exactly and the autograder didint like that
