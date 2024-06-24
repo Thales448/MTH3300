9d9a30b8863d3155fd196d515a8b2fb3da4a01bc
Thales448 <atomicabsurdityreigns@gmail.com>
2024-06-24 22:21:05 +0000

    yes

---
+++ population.py
@@ -0,0 +1,14 @@
+"""
+Population Homework: Calculate the population growth given changing intervals of time and growth rate. 
+Cumulatively using only three variables
+"""
+import math
+
+print("Welcome to the Population Calculator\n Start by inputing your initial Population")
+P = int(input("Population:"))
+rtsums = 0
+for num in range(3):
+    place = ['first', 'second', 'third'] 
+    rtsums +=(float(input(f"Enter {place[num]} time period (in years): "))* (float(input(f'Enter {place[num]} growth rate (in percentage): '))/100))
+print(f'The final population is {P*math.exp(rtsums)})
+    
